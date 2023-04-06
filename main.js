76c58475499d4674594527a060f5e5282534a86c
odette <113144923+caroline-13@users.noreply.github.com>
2023-04-06 16:56:46 -0600

    modificacion de tamaño

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var nariz_roja="";
 var gato="";
 var ojo_derechox=0;
 var ojo_izquierdox=0;
+var diferencia =0;
 function preload(){
     nariz_roja=loadImage("https://i.postimg.cc/7ZBcjDqp/clownnose.png");
 gato=loadImage("https://creazilla-store.fra1.digitaloceanspaces.com/emojis/42100/weary-cat-emoji-clipart-md.png")
@@ -16,24 +17,25 @@ function setup(){
     video.size(300,300);
     video.hide();
     poses = ml5.poseNet(video,listo);
-    poses.on("poses",obtenerPoses);
+    poses.on("pose",obtenerPoses);
 }
 function draw(){
    image(video,0,0,300,300);
    circle(narizx,narizy,20);
    image(nariz_roja,narizx -20,narizy -20 ,40,40);
-   image(gato,narizx -20,narizy -20 ,diferencia,diferencia);
+   tamaño=diferencia*3;
+   image(gato,narizx - (tamaño/2),narizy -(tamaño/2) ,tamaño,tamaño);
 }
 function listo(){
     console.log("estoy listo 😸");
 }
 function obtenerPoses(resultado){
-if(resultados.legth > 0){
+if(resultado.length > 0){
     narizx = resultado[0].pose.nose.x;
     narizy = resultado[0].pose.nose.y;
     ojo_derechox = resultado[0].pose.rightEye.x;
     ojo_izquierdox = resultado[0].pose.leftEye.x;
-    diferencia = ojo_derechox - ojo_izquierdox;
+    diferencia = ojo_izquierdox - ojo_derechox;
     console.log(diferencia);
 }
 }
